73b33258dd1514e7beee2ee251470041fd64e87f
win <fancodeexpert@outlook.com>
2019-09-07 02:43:46 -0400

    realigned header with space-between

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,22 +10,26 @@ export default class Header extends Component {
 
   render() {
     return (
-      <div>
-        <Navbar style={{ justifyContent: "right", backgroundColor: "#2f2fa2" }}>
-          <Navbar.Brand href="/" style={{ fontWeight: "bold", color: "white" }}>
-            Home <i className="fas fa-leaf" style={{ color: "#46CB18" }}></i>
-          </Navbar.Brand>
+      <Navbar
+        style={{ justifyContent: "space-between", backgroundColor: "#2f2fa2" }}
+      >
+        <Navbar.Brand href="/" style={{ fontWeight: "bold", color: "white" }}>
+          Home <i className="fas fa-leaf" style={{ color: "#46CB18" }}></i>
+        </Navbar.Brand>
 
+        <div
+          className="headerLogo"
+          style={{
+            textAlign: "center",
+            color: ""
+          }}
+        >
+          {" "}
           <div
-            className="headerLogo"
             style={{
-              textAlign: "center",
-              color: "",
-
-              padding: 5
+              textAlign: "center"
             }}
           >
-            {" "}
             <span style={{ color: "white", fontWeight: "bold" }}>
               <span></span>
               Nature's<span style={{ padding: 5 }}></span>
@@ -34,36 +38,30 @@ export default class Header extends Component {
               Landscaping
             </span>
           </div>
+        </div>
 
-          <Nav className="ml-auto">
-            <Nav.Link
-              href="/work"
-              style={{ color: "white", fontWeight: "bold" }}
-            >
-              Services
-            </Nav.Link>
-            <Nav.Link
-              href="/work"
-              style={{ color: "white", fontWeight: "bold" }}
-            >
-              Gallery
-            </Nav.Link>
+        <Nav>
+          <Nav.Link href="/work" style={{ color: "white", fontWeight: "bold" }}>
+            Services
+          </Nav.Link>
+          <Nav.Link href="/work" style={{ color: "white", fontWeight: "bold" }}>
+            Gallery
+          </Nav.Link>
 
-            <Nav.Link
-              style={{ color: "white", fontWeight: "bold" }}
-              href="/about"
-            >
-              About
-            </Nav.Link>
-            <Nav.Link
-              href="/contact"
-              style={{ color: "white", fontWeight: "bold" }}
-            >
-              Contact
-            </Nav.Link>
-          </Nav>
-        </Navbar>
-      </div>
+          <Nav.Link
+            style={{ color: "white", fontWeight: "bold" }}
+            href="/about"
+          >
+            About
+          </Nav.Link>
+          <Nav.Link
+            href="/contact"
+            style={{ color: "white", fontWeight: "bold" }}
+          >
+            Contact
+          </Nav.Link>
+        </Nav>
+      </Navbar>
     );
   }
 }
