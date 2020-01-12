81130a9305ab93d8c75fd1841db5d9b36e91fafc
win <fancodeexpert@outlook.com>
2020-01-12 16:13:43 -0500

    added navbar

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,47 +1,38 @@
 import React, { Component } from "react";
-import { Nav, Navbar } from "react-bootstrap";
+import { Nav, Navbar, NavDropdown } from "react-bootstrap";
 import "./header.styles.scss"
-import logo from "../../assets/logo.png"
+import logo from "../../assets/logo1.png"
 
 export default class Header extends Component {
   render() {
-
-
     if (window.location.pathname === '/') return null;
-
-
     return (
       <div style={{}}>
-        <Navbar
-          style={{ justifyContent: "right", backgroundColor: "transparent" }}
-        >
-          <Navbar.Brand href="/" style={{ color: "#A9A9A9" }}>
-            <div className="logo mr-auto ml-auto">
-              <div
-                className="logo-image"
-                style={{
-                  backgroundImage: `url(
-            ${logo}
-          )`,
-                  backgroundRepeat: "no-repeat",
 
-                }}
-              /></div>
+        <Navbar collapseOnSelect expand="lg" >
+          <Navbar.Brand href="/home"><img src={logo} style={{ height: "9vh" }} /></Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="mr-auto">
+              <NavDropdown title="Services" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Landscaping</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">Property Management</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">Trash Removal</NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#features">About</Nav.Link>
+              <Nav.Link href="#pricing">Gallery</Nav.Link>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">Contact</Nav.Link>
+              <Nav.Link eventKey={2} href="#memes">
+                Login
+      </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
 
-          </Navbar.Brand>
-
-          <Nav className="ml-auto">
-            <Nav.Link href="/artists"  >
-              Services
-            </Nav.Link>
-            <Nav.Link href="/about" >
-              About
-            </Nav.Link>
-            <Nav.Link href="/login" >
-              Login
-            </Nav.Link>
-          </Nav>
-        </Navbar >
       </div >
     );
   }
