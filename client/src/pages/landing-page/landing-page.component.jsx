40d0a1a4efe499320357c91472d5188fceea7ab4
win <fancodeexpert@outlook.com>
2020-01-11 23:24:45 -0500

    -more site styling

--- client/src/pages/landing-page/landing-page.component.jsx
+++ client/src/pages/landing-page/landing-page.component.jsx
@@ -13,20 +13,12 @@ const LandingPage = () => (
   <div style={{ justifyContent: "center", alignItems: "center" }}>
     <div className="backgroundImage" style={{ width: "100vw", height: "100vh" }}>
       <div className="container">
-        <span className="companyName">Nature’s <span className="frontier">Frontier</span> Landscaping
-
-& Property Management LLC</span>
+        <span className="companyName">Nature’s <span className="frontier">Frontier</span> Landscaping & Property Management LLC</span>
         <span className="quote">“We see our customers as invited guests to a party, and we are the hosts. It’s our job to make the customer experience a little bit better.”</span>
         <span className="quoteSign">-Jeff Bezos, Founder of Amazon</span>
-        <div><Button>Join the party</Button></div>
+        <div><Button className="landingButton" onClick={() => window.location.href = "http://nflandscaping.com/home"}>Join the party</Button></div>
       </div>
-
     </div>
-
-    {/* 
-    <div style={{ marginTop: 30 }}>
-      <Signature />
-    </div> */}
   </div>
 
 );
