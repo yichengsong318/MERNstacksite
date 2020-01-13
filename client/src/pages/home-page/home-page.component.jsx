36f4bcbe7413b844d10b25087df40a41b1e72f14
win <fancodeexpert@outlook.com>
2020-01-13 18:59:52 -0500

    added buttons

--- client/src/pages/home-page/home-page.component.jsx
+++ client/src/pages/home-page/home-page.component.jsx
@@ -3,6 +3,7 @@ import "./home-page.styles.scss";
 // import ContentGrid from "../../components/grid/grid-component";
 import Signature from "../../components/common/signature/signature.component";
 import logo from "../../assets/logo1.png"
+import { Button } from 'react-bootstrap'
 
 const HomePage = () => (
     <div className="home-container" style={{ justifyContent: "center", alignItems: "center" }}>
@@ -13,10 +14,10 @@ const HomePage = () => (
             <span>Monday - Friday 8:00AM - 6:00PM</span>
             <span>Serving Rhode Island and Mass</span>
         </div>
-        <div style={{ fontWeight: "bold", color: "white", fontSize: 50 }}>
-            <p>Site Under Construction</p>
-
+        <div className="button-container">
+            <Button>Contact Us</Button>   <Button>Free Estimate</Button>
         </div>
+
 
         <div className="signature">
             <Signature />
