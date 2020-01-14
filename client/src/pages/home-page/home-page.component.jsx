42e249499faa473fa2a353b9f857da3f6a328356
win <fancodeexpert@outlook.com>
2020-01-13 19:11:38 -0500

    added sliding background images

--- client/src/pages/home-page/home-page.component.jsx
+++ client/src/pages/home-page/home-page.component.jsx
@@ -4,24 +4,33 @@ import "./home-page.styles.scss";
 import Signature from "../../components/common/signature/signature.component";
 import logo from "../../assets/logo1.png"
 import { Button } from 'react-bootstrap'
+import BackgroundSlider from 'react-background-slider'
+import homeBackground from "../../assets/homeBackground.jpg"
 
 const HomePage = () => (
     <div className="home-container" style={{ justifyContent: "center", alignItems: "center" }}>
+        <div style={{ backgroundSize: "contain" }}>
+            <BackgroundSlider
+                images={[homeBackground]}
+                duration={10} transition={2} />
 
-        <img src={logo} className="logo-home" />
-        <div className="containerContact">
-            <span><i class="fas fa-phone"></i> 401-447-5446</span>
-            <span>Monday - Friday 8:00AM - 6:00PM</span>
-            <span>Serving Rhode Island and Mass</span>
-        </div>
-        <div className="button-container">
-            <Button>Contact Us</Button>   <Button>Free Estimate</Button>
+            {/* <img src={logo} className="logo-home" />
+            <div className="containerContact">
+                <span><i class="fas fa-phone"></i> 401-447-5446</span>
+                <span>Monday - Friday 8:00AM - 6:00PM</span>
+                <span>Serving Rhode Island and Mass</span>
+            </div>
+            <div className="button-container">
+                <Button>Contact Us</Button>   <Button>Free Estimate</Button>
+            </div>
+
+
+            <div className="signature">
+                <Signature />
+            </div> */}
         </div>
 
 
-        <div className="signature">
-            <Signature />
-        </div>
     </div>
 );
 export default HomePage;
