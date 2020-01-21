2fc2fac588ea65d6b114c6d97b2a49f56e7010d6
win <fancodeexpert@outlook.com>
2020-01-20 21:23:08 -0500

    moved nav bar to middle on mobile devices and outside collapsible nav bar

--- client/src/components/header/header.component.jsx
+++ client/src/components/header/header.component.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
 import { Nav, Navbar, NavDropdown } from "react-bootstrap";
-import "./header.styles.scss"
+import "./header.styles.scss";
 
 export default class Header extends Component {
   render() {
-    if (window.location.pathname === '/') return null;
+    if (window.location.pathname === "/") return null;
     return (
       <div>
-
-        <Navbar collapseOnSelect expand="lg" className="navbar-inner" variant="dark" bg="dark">
-          <Navbar.Brand href="/home"><i class="fas fa-home"></i> Home</Navbar.Brand>
+        <Navbar collapseOnSelect expand="lg" className="navbar-inner">
+          <Navbar.Brand href="/home">
+            <i class="fas fa-home"></i>
+          </Navbar.Brand>
+          <NavDropdown title="Services" id="collasible-nav-dropdown">
+            <NavDropdown.Item href="/landscaping">Landscaping</NavDropdown.Item>
+            <NavDropdown.Item href="/property-management">
+              Property Management
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/trash-removal">
+              Trash Removal
+            </NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item href="#action/3.4">
+              Separated link
+            </NavDropdown.Item>
+          </NavDropdown>
           <Navbar.Toggle aria-controls="responsive-navbar-nav" />
           <Navbar.Collapse id="responsive-navbar-nav">
             <Nav className="mr-auto">
-              <NavDropdown title="Services" id="collasible-nav-dropdown" bg="dark">
-                <NavDropdown.Item href="/landscaping">Landscaping</NavDropdown.Item>
-                <NavDropdown.Item href="/property-management">Property Management</NavDropdown.Item>
-                <NavDropdown.Item href="/trash-removal">Trash Removal</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-              </NavDropdown>
               <Nav.Link href="/about">About Us</Nav.Link>
               <Nav.Link href="/gallery">Gallery</Nav.Link>
             </Nav>
@@ -27,12 +34,11 @@ export default class Header extends Component {
               <Nav.Link href="/contact">Contact</Nav.Link>
               <Nav.Link eventKey={2} href="#memes">
                 Login
-      </Nav.Link>
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Navbar>
-
-      </div >
+      </div>
     );
   }
 }
