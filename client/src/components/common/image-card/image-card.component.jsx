cee970b9d0bb5fe0d1b009618e8ae86cd0115660
win <fancodeexpert@outlook.com>
2020-01-30 22:03:32 -0500

    created reusbale boostrap card component to display services

---
+++ client/src/components/common/image-card/image-card.component.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const ImageCard = () => {
+  return (
+    <div>
+      <Card>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+          <Card.Text>
+            Some quick example text to build on the card title and make up the
+            bulk of the card's content.
+          </Card.Text>
+        </Card.Body>
+      </Card>
+    </div>
+  );
+};
+
+export default ImageCard;
--- client/src/pages/services-page/services-page.component.jsx
+++ client/src/pages/services-page/services-page.component.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import "./services-pages.styles.scss";
+import ImageCard from "../../components/common/image-card/image-card.component";
 
 const ServicePage = () => {
-  return <div>Services</div>;
+  return (
+    <div>
+      <span>Services</span>
+      <ImageCard />
+    </div>
+  );
 };
 
 export default ServicePage;
