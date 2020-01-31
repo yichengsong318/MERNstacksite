2ff237f5413729e1ef4340295fb2d3c674f5b977
win <fancodeexpert@outlook.com>
2020-01-30 23:43:28 -0500

    updated contact page

--- client/src/pages/contact-page/contact-page.component.jsx
+++ client/src/pages/contact-page/contact-page.component.jsx
@@ -3,6 +3,7 @@ import ContactForm from "../../components/contact-form/contact-form.component";
 import "./contact-page.styles.scss";
 import Fade from "react-reveal/Fade";
 import Flip from "react-reveal/Flip";
+import { Card, ListGroup } from "react-bootstrap";
 
 const ContactPage = () => {
   return (
@@ -11,19 +12,34 @@ const ContactPage = () => {
         <p className="contact-header">Contact Us</p>
       </Fade>
       <div style={{ padding: 10 }}></div>
-      <div className="message-us">
-        <Fade top>
-          <div style={{}}>
-            <span className="send-us-email">Send us an email...</span>
-          </div>
-        </Fade>
-        <p className="company-email">
-          <span style={{ marginRight: 5, fontWeight: "bold" }}>
-            <i class="far fa-envelope"></i>
-          </span>
-          Naturesfrontierlandscaping@gmail.com
-        </p>
-      </div>
+      <Fade top>
+        <Card
+          className="contact-list"
+          style={{ width: "25rem", maxWidth: "90vw" }}
+        >
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <a
+                href="tel:401-447-5446"
+                data-rel="external"
+                style={{ color: "black" }}
+              >
+                <i class="fas fa-phone"></i> 401-447-5446
+              </a>
+            </ListGroup.Item>
+            <ListGroup.Item>
+              <span style={{ marginRight: 3, fontWeight: "bold" }}>
+                <i class="far fa-envelope"></i>
+              </span>
+              naturesfrontierlandscaping@gmail.com
+            </ListGroup.Item>
+            <ListGroup.Item style={{ fontWeight: "bold" }}>
+              Available 24/7
+            </ListGroup.Item>
+          </ListGroup>
+        </Card>
+      </Fade>
+
       <Flip left>
         <div style={{ marginTop: 80 }}>
           <span className="send-us-email" style={{}}>
@@ -33,6 +49,14 @@ const ContactPage = () => {
       </Flip>
 
       <ContactForm />
+      <div style={{ paddingBottom: 30 }}></div>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2973.3856875883093!2d-71.35262728526868!3d41.81999237922684!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89e45b7fe89d3c6b%3A0x21555fc05f23db39!2sNatures%20Frontier%20Landscaping%20Construction%20%26%20Property%20Management!5e0!3m2!1sen!2sus!4v1580445738200!5m2!1sen!2sus"
+        width="300"
+        height="300"
+        frameborder="0"
+        allowfullscreen=""
+      ></iframe>
       <div style={{ paddingBottom: 200 }}></div>
     </div>
   );
