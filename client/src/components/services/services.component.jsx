92d668471464e6418121a8829cda8750efc28fb7
win <fancodeexpert@outlook.com>
2020-01-30 23:14:28 -0500

    added animation to service cards when they render

--- client/src/components/services/services.component.jsx
+++ client/src/components/services/services.component.jsx
@@ -1,152 +1,155 @@
 import React from "react";
 import { Card } from "react-bootstrap";
 import "./services.styles.scss";
+import Fade from "react-reveal/Fade";
 
 const Services = () => {
   return (
-    <div className="services-container">
-      <Card className="menu-item">
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body>
-          <Card.Title>Full Landscaping Services</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="menu-item" style={{}}>
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body style={{}}>
-          <Card.Title>Property Management</Card.Title>
-          <Card.Text style={{ minHeight: "300" }}>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
+    <Fade top cascade>
+      <div className="services-container">
+        <Card className="menu-item">
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body>
+            <Card.Title>Full Landscaping Services</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="menu-item" style={{}}>
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body style={{}}>
+            <Card.Title>Property Management</Card.Title>
+            <Card.Text style={{ minHeight: "300" }}>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-      <Card className="menu-item" style={{}}>
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body>
-          <Card.Title className="title">Snow Removal</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="menu-item" style={{}}>
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body>
-          <Card.Title>Trash Removal</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="menu-item" style={{}}>
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body>
-          <Card.Title>Bobcat Services</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="menu-item" style={{}}>
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body>
-          <Card.Title>Power Washing</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="menu-item" style={{}}>
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body>
-          <Card.Title>Consulting</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="menu-item" style={{}}>
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body>
-          <Card.Title>Irrigation</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="menu-item" style={{}}>
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body>
-          <Card.Title>Fencing</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-      <Card className="menu-item" style={{}}>
-        <Card.Img
-          className="background-image"
-          variant="top"
-          src={require("../../assets/landscaping-service.jpg")}
-        />
-        <Card.Body>
-          <Card.Title>Home Improvement</Card.Title>
-          <Card.Text>
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </Card.Text>
-        </Card.Body>
-      </Card>
-    </div>
+        <Card className="menu-item" style={{}}>
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body>
+            <Card.Title className="title">Snow Removal</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="menu-item" style={{}}>
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body>
+            <Card.Title>Trash Removal</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="menu-item" style={{}}>
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body>
+            <Card.Title>Bobcat Services</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="menu-item" style={{}}>
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body>
+            <Card.Title>Power Washing</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="menu-item" style={{}}>
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body>
+            <Card.Title>Consulting</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="menu-item" style={{}}>
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body>
+            <Card.Title>Irrigation</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="menu-item" style={{}}>
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body>
+            <Card.Title>Fencing</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="menu-item" style={{}}>
+          <Card.Img
+            className="background-image"
+            variant="top"
+            src={require("../../assets/landscaping-service.jpg")}
+          />
+          <Card.Body>
+            <Card.Title>Home Improvement</Card.Title>
+            <Card.Text>
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </Card.Text>
+          </Card.Body>
+        </Card>
+      </div>
+    </Fade>
   );
 };
 
