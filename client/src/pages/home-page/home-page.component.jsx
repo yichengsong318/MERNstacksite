990fa6c9073694d4ea3aa8b1071ecb25acb95bc3
win <fancodeexpert@outlook.com>
2020-02-16 14:28:46 -0500

    old code removal

--- client/src/pages/home-page/home-page.component.jsx
+++ client/src/pages/home-page/home-page.component.jsx
@@ -111,67 +111,6 @@ const HomePage = () => {
           ))}
         </Slider>
       </div>
-      {/* <div style={{ backgroundSize: "contain" }}>
-          <div>
-            <img src={logo} className="logo-home" />
-            <div className="containerContact">
-              <span>Monday - Sunday 24/7</span>
-              <span>Available for emergencies</span>
-              <span style={{ fontWeight: "bold" }}>Serving RI and Mass</span>
-              <span>401-447-5446</span>
-            </div>
-          </div>
-
-          <div className="button-container">
-            <Button>
-              <a
-                href="tel:401-447-5446"
-                data-rel="external"
-                style={{ color: "white" }}
-              >
-                <i class="fas fa-phone"></i> 401-447-5446
-              </a>
-            </Button>
-            <Button
-              style={{ fontWeight: "bold" }}
-              onClick={() => (window.location.href = "/contact")}
-            >
-              Free Estimate
-            </Button>
-          </div>
-        </div> */}
-
-      {/* <Fade right>
-        <div style={{ backgroundColor: "#CDCDCD" }}>
-          <div style={{ padding: 10 }}> </div>
-          <div>
-            <span class="what-we-do-header">What we do</span>
-          </div>
-
-          <div>
-            <span class="what-we-do-sub-head"> Licensed and fully insured</span>
-          </div>
-
-          <div style={{ padding: 10 }}> </div>
-          <LazyLoad height={200} placeholder={<Spinner />} offset={100}>
-            <WhatWeDo />
-          </LazyLoad>
-
-          <div style={{ padding: 30 }}></div>
-        </div>
-      </Fade>
-      <Fade left>
-        <div style={{ backgroundColor: "#343A40" }}>
-          <div style={{ padding: 40 }}> </div>
-
-          <div>
-            <span class="message-header">Send us a message!</span>
-          </div>
-
-          <ContactForm />
-          <div style={{ padding: 30 }}></div>
-        </div>
-      </Fade> */}
     </div>
   );
 };
