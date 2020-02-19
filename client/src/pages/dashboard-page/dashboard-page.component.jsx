2761e6c9093128905639cd317f4bb3ec6a12c522
win <fancodeexpert@outlook.com>
2020-02-18 21:34:53 -0500

    removed unused code on private dashboard

--- client/src/pages/dashboard-page/dashboard-page.component.jsx
+++ client/src/pages/dashboard-page/dashboard-page.component.jsx
@@ -1,23 +1,9 @@
 import React from "react";
-// import logo from "../../assets/logo.PNG";
 import "./dashboard-page.styles.scss";
 
 const DashboardPage = () => (
   <div style={{ justifyContent: "center", alignItems: "center" }}>
-    {/* <div className="logo mr-auto ml-auto">
-      <div
-        className="logo-image"
-        style={{
-          backgroundImage: `url(
-            ${logo}
-          )`,
-          backgroundRepeat: "no-repeat"
-        }}
-      />
-    </div> */}
     <p style={{ color: "red", fontWeight: "bold", fontSize: 30 }}>Dashboard</p>
-
-
   </div>
 );
 export default DashboardPage;
