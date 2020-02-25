d9fc3f482508906ad780ba18a4ba2c440b11f07d
win <fancodeexpert@outlook.com>
2020-02-24 21:44:31 -0500

    added home improvement gallery page + scss sheet for home improvement photos

---
+++ client/src/pages/gallery-home-improvement-page/galler-home-improvement.component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./gallery-home-improvement.styles.scss";
+
+const GalleryHomeImprovementPage = () => {
+  return <div>Home improvement</div>;
+};
+
+export default GalleryHomeImprovementPage;
